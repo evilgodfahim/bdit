148f1f6bd9d1deccc5a2e01d3bd53d08aacc65b4
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-11-07 00:25:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ FEEDS = [
     "https://evilgodfahim.github.io/sop/opinion_feed.xml",  # মতামত-সমকাল
     "https://politepol.com/fd/tqu8P8uIlNm1.xml",  # বিষয় – DW
     "https://feeds.bbci.co.uk/bengali/rss.xml",  # BBC Bangla - মূলপাতা
+
+"https://politepol.com/fd/YgbESpqhLwdK.xml"
 ]
 
 MASTER_FILE = "feed_master.xml"
