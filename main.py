2cf8fbfeeed27b15da4f7cb129799742193ea421
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-11-08 23:16:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,11 @@ FEEDS = [
 "https://politepol.com/fd/YgbESpqhLwdK.xml",
 "https://politepol.com/fd/TnjwLaSLd1M8.xml",
 "https://politepol.com/fd/e0zKTeKoRpXa.xml",
-"https://evilgodfahim.github.io/kk/opinion.xml"
+"https://evilgodfahim.github.io/kk/opinion.xml",
+"https://politepol.com/fd/wpC8lyjp7CVq.xml",
+"https://politepol.com/fd/1yC3YJpL3i6t.xml",
+"https://politepol.com/fd/DbgDjmR4B0ua.xml",
+"https://politepol.com/fd/aPXIv1Q7cs7S.xml"
 ]
 
 MASTER_FILE = "feed_master.xml"
