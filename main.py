8f0cad18b9bbbd0f825880c93a2003c65cd52af4
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-11-16 11:12:00 +0600

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,8 @@ FEEDS = [
 "https://politepol.com/fd/RW7B9eQ8SuQ8.xml",
 "https://politepol.com/fd/Om635UbkdlGQ.xml",
 "https://politepol.com/fd/iBikrmLHw51t.xml",
-"https://politepol.com/fd/joNpOlIQpxws.xml"
+"https://politepol.com/fd/joNpOlIQpxws.xml",
+"https://politepol.com/fd/xwWyLagKzYe1.xml"
 ]
 
 MASTER_FILE = "feed_master.xml"
