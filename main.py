ab69b623d301fdf0d0a03372a50ea1e062e484e9
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-12-05 09:11:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ FEEDS = [
     "https://evilgodfahim.github.io/pb/articles.xml",
 "https://politepol.com/fd/bdnPXYy1YR1g.xml",
 "https://evilgodfahim.github.io/bt/columns.xml"
+"https://politepol.com/fd/l7Izgmv6b2LN.xml"
 ]
 
 MASTER_FILE = "feed_master.xml"
